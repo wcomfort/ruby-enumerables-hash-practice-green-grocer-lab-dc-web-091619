d9ed5e9502315b8766e68ac89a549814b8e74588
Will Comfort <comfortwb21@gmail.com>
2019-09-06 21:28:24 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -39,5 +39,6 @@ end
 def checkout(cart, coupons)
   hash_cart = consolidate_cart(cart)
   applied_coupons = apply_coupons(hash_cart, coupons)
-  applied_discount = apply_clearance()
+  applied_discount = apply_clearance(applied_coupons)
+  
 end

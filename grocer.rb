7d34fcdfbc059111010ad022b9e3c6536fa7bc40
Will Comfort <comfortwb21@gmail.com>
2019-09-06 21:34:03 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,5 +41,5 @@ def checkout(cart, coupons)
   applied_coupons = apply_coupons(hash_cart, coupons)
   applied_discount = apply_clearance(applied_coupons)
   total = applied_discount.reduce(0) {|acc, (key, value)| acc += value[:price] * value[:count]}
-  total > 100 ?  
+  total > 100 ? total * 0.9 : total  
 end
